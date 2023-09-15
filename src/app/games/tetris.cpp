b54d7db54879b23864351107a8051efb896644c6
stanislavsn1 <stanislavsn1@verifone.com>
2023-09-15 15:58:10 +0300

    Started tetris implementation

---
+++ src/app/games/tetris.cpp
@@ -0,0 +1,19 @@
+#include "tetris.h"
+
+namespace MiniG::Games
+{
+void Tetris::OnAttach()
+{
+
+}
+
+void Tetris::OnUpdate()
+{
+
+}
+
+void Tetris::OnDetach()
+{
+
+}
+} /* namespace MiniG::Games */
---
+++ src/app/games/tetris.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "game.h"
+#include "app/common.h"
+
+#include <array>
+
+namespace MiniG::Games
+{
+class Tetris;
+class Tetramino
+{
+	std::vector<MGVec2<int>> OccupiedCells = {};
+
+	friend Tetris;
+};
+/*
+	Possible figures(tetraminos):
+	                          
+	[]      []    []      [][]
+	[]      []    []      [][]
+	[]      []    []          
+	[]    [][]    [][]        
+	                          
+	  [][]    [][][]    [][]  
+	[][]        []        [][]
+	                          
+	Each of them can be rotated 90 degrees
+*/
+class Tetris : public Game
+{
+	public:
+		Tetris() : Game() {}
+
+		virtual void OnAttach();
+		virtual void OnUpdate();
+		virtual void OnDetach();
+	
+	public:
+		const std::string m_GameName = "Tetris";
+
+	private:
+		/* Tetris field is 10 x 20 */
+		/* TODO: Idk how really handle this, do we need smth more than a bool per cell? */
+		/* Maybe makes sense to have a vector, so when we clear a line we could just remove 1 array from double array? */
+		/* Also if nothing more than bool is not needed, than we can use int and set appropriate bits to each cell */
+		std::array<std::array<bool, 10>, 20> m_Field = {};
+
+		Tetramino m_FallingTetramino;
+};
+} /* namespace MiniG::Games */
