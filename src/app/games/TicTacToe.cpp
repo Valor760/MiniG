b07bd6574e10dcdf54d0e86907b3ffeb3a9e9613
ALex <aleksej.nikiforov97@gmail.com>
2023-09-24 21:03:50 +0300

    Test

--- src/app/games/TicTacToe.cpp
+++ src/app/games/TicTacToe.cpp
@@ -4,7 +4,7 @@ namespace MiniG::Games
 {
 
     void TicTacToe:: OnAttach(){
-        LOG_DEBUG("ataching TicTac");
+        LOG_DEBUG("ataching TicTacToe");
     }
 	void TicTacToe:: OnUpdate(double dt){}
 	void TicTacToe:: OnDetach(){}
